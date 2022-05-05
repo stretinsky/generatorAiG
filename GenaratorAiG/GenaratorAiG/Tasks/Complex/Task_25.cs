e2a62a7365b330114d1403b36df7c35a66a4cb75
Александр Кузнецов <kuznecovaleksandr923@gmail.com>
2022-05-05 13:18:37 +0400

    Task 25 и 26

---
+++ GenaratorAiG/GenaratorAiG/Tasks/Complex/Task_25.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenaratorAiG.Tasks.Complex
+{
+    internal class Task_25
+    {
+        string description = "Решить квадратное уравнение:";
+        System.Numerics.Complex complex1, complex2, discriminant, z1, z2;
+        Random rnd = new Random();
+
+        public Task_25()
+        {
+            //Можно убрать System.Numerics (у меня using не работал просто)
+            complex1 = new System.Numerics.Complex(rnd.Next(1,10), rnd.Next(-5, 5));
+            complex2 = new System.Numerics.Complex(rnd.Next(1, 10), rnd.Next(-5, 5));
+
+            discriminant = System.Numerics.Complex.Sqrt(complex1 * complex1 - 4 * complex2);
+
+            z1 = (-complex1 + discriminant) / 2;
+            z1 = (- complex1 - discriminant) / 2;
+        }
+
+        public string GetDescription()
+        {
+            return description;
+        }
+        public string GetCondition()
+        {
+            return $"$z^2$ + z * ({complex1}) + ({complex2}) = 0";
+        }
+        public string GetAnswer()
+        {
+            return $"$z_1$ = \\frac{{{-complex1} + {discriminant}}} {{{2}}} = {z1}" + $"\n$z_2$ = \\frac{{{-complex1} - {discriminant}}} {{{2}}} = {z2}";
+        }
+    }
+}
---
+++ GenaratorAiG/GenaratorAiG/Tasks/Complex/Task_26.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenaratorAiG.Tasks.Complex
+{
+    internal class Task_26
+    {
+        string description = "Решить уравнение:";
+        System.Numerics.Complex complex;
+        int degree;
+        Random rnd = new Random();
+
+        public Task_26()
+        {
+            degree = rnd.Next(2, 6);
+            complex = new System.Numerics.Complex(1, 1);
+
+            //Корень degree-степени
+            complex = System.Numerics.Complex.Pow(complex, 1/degree);
+        }
+
+        public string GetDescription()
+        {
+            return description;
+        }
+        public string GetCondition()
+        {
+            return $"$z^{degree}$ - 1 = {complex.Imaginary}";
+        }
+        public string GetAnswer()
+        {
+            return $"z = {complex}";
+        }
+    }
+}
