6854e9719ce8621f3ac3c740bbae7e1c91e2a2ec
Danil <danils0393@gmail.com>
2022-05-18 20:44:27 +0600

    + tasks added

--- GenaratorAiG/GenaratorAiG/Form1.Designer.cs
+++ GenaratorAiG/GenaratorAiG/Form1.Designer.cs
@@ -47,7 +47,7 @@ namespace GenaratorAiG
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
