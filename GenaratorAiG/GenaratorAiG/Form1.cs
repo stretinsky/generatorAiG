d5c240172fd5afcdd6f15124d91b58eb48e47c18
Александр Кузнецов <kuznecovaleksandr923@gmail.com>
2022-05-19 17:07:48 +0400

    Task 82 добавлен Полностью протестирован

--- GenaratorAiG/GenaratorAiG/Form1.cs
+++ GenaratorAiG/GenaratorAiG/Form1.cs
@@ -9,9 +9,9 @@ using System.IO;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Latex;
-using PdfSharp;
-using PdfSharp.Pdf;
-using TheArtOfDev.HtmlRenderer.PdfSharp;
+//using PdfSharp;
+//using PdfSharp.Pdf;
+//using TheArtOfDev.HtmlRenderer.PdfSharp;2
 using GenaratorAiG.Tasks.SLAE;
 using GenaratorAiG.Tasks.Complex;
 using System.Threading;
@@ -32,8 +32,8 @@ namespace GenaratorAiG
         {
             try
             {
-           
-                string latex = @"\frac{23}{5}"; // здесь ваша латех формула  
+                Task_82 task1 = new Task_82();
+                string latex = task1.GetCondition() + "\\\\" + task1.GetAnswer(); // здесь ваша латех формула  
 
                 pictureBox1.Image = latexHandler.CreateLatexImage(latex);
 
