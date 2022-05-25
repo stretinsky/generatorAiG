3fa36423c6cbe68f06f7cb1d7bdfb611cefe173f
Александр Кузнецов <kuznecovaleksandr923@gmail.com>
2022-05-25 22:44:33 +0400

    Удалил ненужные Usingи

--- GenaratorAiG/GenaratorAiG/Tasks/Analytic geometry/Task_119.cs
+++ GenaratorAiG/GenaratorAiG/Tasks/Analytic geometry/Task_119.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using LinearAlgebraAndCo;
-using Fractions;
 
 namespace GenaratorAiG.Tasks.Analytic_geometry
 {
